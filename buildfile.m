376aec2fc45ff7066a85b4c1663a626f5df7db78
Tushar Sharma <tsharma@mathworks.com>
2023-12-08 16:07:59 +0530

    modify buildFile

---
+++ buildfile.m
@@ -0,0 +1,11 @@
+function plan = buildfile
+import matlab.buildtool.tasks.CodeIssuesTask
+import matlab.buildtool.tasks.TestTask
+
+% Create a plan from task functions
+plan = buildplan(localfunctions);
+
+% Add a task to identify code issues
+plan("check") = CodeIssuesTask;
+
+end
