fce82343a718eec2b2a6fe792b5fbe9f72627202
Tushar Sharma <tsharma@mathworks.com>
2023-12-11 12:43:47 +0530

    change test

--- arrayProductTest.m
+++ arrayProductTest.m
@@ -1,12 +1,12 @@
 classdef arrayProductTest < matlab.unittest.TestCase
     methods(Test)
-        function checkCalculationResult(testCase)
+        function validateCalculationResult(testCase)
             actSolution = arrayProduct(5,[1.5, 2, 9]);
             expSolution = [7.5, 10, 45];
             testCase.verifyEqual(actSolution,expSolution)
         end
-        function checkNotRowVector(testCase)
-            testCase.verifyError(@()arrayProduct(2,[1 2;3 4]), "MyToolbox:arrayProduct:notRowVector");
+        function validateNumberOfInputs(testCase)
+            testCase.verifyError(@()arrayProduct(2), "MyToolbox:arrayProduct:nrhs");
         end
     end
 end
