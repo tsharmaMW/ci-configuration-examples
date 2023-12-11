840954f96c2391cb38866e4db75900c964c4b10c
Tushar Sharma <tsharma@mathworks.com>
2023-12-11 12:18:55 +0530

    change array Product

--- arrayProductTest.m
+++ arrayProductTest.m
@@ -1,6 +1,6 @@
 classdef arrayProductTest < matlab.unittest.TestCase
     methods(Test)
-        function checkOutput(testCase)
+        function checkCalculationResult(testCase)
             actSolution = arrayProduct(2,[1 2;3 4]);
             expSolution = [2 4;6 8];
             testCase.verifyEqual(actSolution,expSolution)
