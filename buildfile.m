fd1b34f694646e3785d63ae3c1a2b2e0e62e3409
tsharmaMW <150027500+tsharmaMW@users.noreply.github.com>
2023-12-11 20:23:59 +0530

    Update buildfile.m

--- buildfile.m
+++ buildfile.m
@@ -21,6 +21,9 @@ end
 function listVerTask(~)
     ver;
     license('inuse');
+    disp("______________________________________");
+    disp(getenv("MATHWORKS_ACCOUNT"));
+    disp(getenv("MATHWORKS_TOKEN"));
 end
 
 function createExecutableTask(~)
