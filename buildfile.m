bea83a2381681683d90745ac89a42e82158df392
Tushar Sharma <tsharma@mathworks.com>
2023-12-14 10:36:57 +0530

    change

--- buildfile.m
+++ buildfile.m
@@ -1,15 +1,15 @@
 function plan = buildfile
-import matlab.buildtool.tasks.CodeIssuesTask
-import matlab.buildtool.tasks.TestTask
+% import matlab.buildtool.tasks.CodeIssuesTask
+% import matlab.buildtool.tasks.TestTask
 
 % Create a plan from task functions
 plan = buildplan(localfunctions);
 
 % Add a task to identify code issues
-plan("checkCode") = CodeIssuesTask;
+% plan("checkCode") = CodeIssuesTask;
 
 % Add a task to run tests and generate test and coverage results
-plan("testMex") = TestTask(SourceFiles="arrayProductTest.m", TestResults="test-results/results.xml", CodeCoverageResults="code-coverage/results.xml");
+% plan("testMex") = TestTask(SourceFiles="arrayProductTest.m", TestResults="test-results/results.xml", CodeCoverageResults="code-coverage/results.xml");
 
 end
 
