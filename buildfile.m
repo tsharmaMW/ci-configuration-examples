dfa49e9fdfb4070825c33f453e8714cfb11aeb13
Tushar Sharma <tsharma@mathworks.com>
2023-12-14 23:01:59 +0530

    add check

--- buildfile.m
+++ buildfile.m
@@ -1,15 +1,15 @@
 function plan = buildfile
-% import matlab.buildtool.tasks.CodeIssuesTask
-% import matlab.buildtool.tasks.TestTask
+import matlab.buildtool.tasks.CodeIssuesTask
+import matlab.buildtool.tasks.TestTask
 
 % Create a plan from task functions
 plan = buildplan(localfunctions);
 
-% Add a task to identify code issues
-% plan("checkCode") = CodeIssuesTask;
+Add a task to identify code issues
+plan("checkCode") = CodeIssuesTask;
 
-% Add a task to run tests and generate test and coverage results
-% plan("testMex") = TestTask(SourceFiles="arrayProductTest.m", TestResults="test-results/results.xml", CodeCoverageResults="code-coverage/results.xml");
+Add a task to run tests and generate test and coverage results
+plan("testMex") = TestTask(SourceFiles="arrayProductTest.m", TestResults="test-results/results.xml", CodeCoverageResults="code-coverage/results.xml");
 
 end
 
@@ -18,12 +18,8 @@ function createMexTask(~)
     mex arrayProduct.c;
 end
 
-function listVerTask(~)
-    ver;
-    license('inuse');
-end
-
 function createExecutableTask(~)
 % Create an executable
     mcc -m code/quadraticSolver.m;
+    dir;
 end
