8309df32c8aa3b60ed6b71827d7228bbc13ad1d8
Tushar Sharma <tsharma@mathworks.com>
2024-04-23 17:20:08 +0530

    update with premade MEX task

--- buildfile.m
+++ buildfile.m
@@ -1,18 +1,17 @@
 function plan = buildfile
 import matlab.buildtool.tasks.TestTask;
+import matlab.buildtool.tasks.MexTask;
 
 % Create a plan from task functions
 plan = buildplan(localfunctions);
+
+% Add a task to build a MEX file
+plan("mex") = MexTask("src/arrayProduct.c","toolbox");
 
 % Add a task to run tests and generate test and coverage results
 plan("test") = TestTask(TestResults="test-results/results.xml");
 plan("test").Dependencies = ["mex"];
 
-end
-
-function mexTask(~)
-    % Create a MEX file
-    mex src/arrayProduct.c -outdir toolbox/;
 end
 
 function packageToolboxTask(~)
@@ -22,8 +21,8 @@ function packageToolboxTask(~)
     toolboxFolder = "toolbox";
     opts = matlab.addons.toolbox.ToolboxOptions(toolboxFolder,identifier);
     
-    opts.ToolboxName = "Cross Platform - Array Product Toolbox";
-    opts.MinimumMatlabRelease = "R2023b";
+    opts.ToolboxName = "Cross-Platform Array Product Toolbox";
+    opts.MinimumMatlabRelease = "R2024a";
 
     matlab.addons.toolbox.packageToolbox(opts);
 end
