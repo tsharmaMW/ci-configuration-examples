2a513ef911a64b658020b155c6a23bad2575c1aa
tsharmaMW <tsharma@mathworks.com>
2024-08-16 17:05:03 +0530

    try integrating cross platform builder to github

---
+++ src/testingFunc.m
@@ -0,0 +1,10 @@
+% Define the multiplier and the input matrix
+multiplier = 2.5;
+inMatrix = [1, 2, 3, 4, 5];
+
+% Call the arrayProduct function
+outMatrix = arrayProduct(multiplier, inMatrix);
+
+% Display the result
+disp('Output Matrix:');
+disp(outMatrix);
