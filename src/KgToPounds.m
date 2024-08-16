5e3eb429a74c7f80491d29489351eeace827e591
tsharmaMW <tsharma@mathworks.com>
2024-08-16 23:33:39 +0530

    update code with mex and legit func

---
+++ src/KgToPounds.m
@@ -0,0 +1,10 @@
+% Define the multiplier and the input matrix
+multiplier = 2.20462262;
+weightInKg = [1, 2, 3, 4, 5];
+
+% Call the arrayProduct function
+weightInPounds = arrayProduct(multiplier, weightInKg);
+
+% Display the result
+disp('Weight in Pounds:');
+disp(weightInPounds);
