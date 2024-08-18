9af8c28e0221168a659404ee42b2ecc8f5ca9681
tsharmaMW <tsharma@mathworks.com>
2024-08-18 17:40:23 +0530

    update config

--- src/KgToPounds.m
+++ src/KgToPounds.m
@@ -1,6 +1,5 @@
 % Define the multiplier and the input matrix
-% multiplier = 2.20462262;
-multiplier = 2;
+multiplier = 2.20462262;
 weightInKg = [1, 2, 3, 4, 5];
 
 % Call the arrayProduct function
