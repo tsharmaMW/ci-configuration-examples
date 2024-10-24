a9ad4236380b20414bc41b4972da20d5ad811076
tsharmaMW <tsharma@mathworks.com>
2024-10-24 09:41:58 +0530

    updata

--- tests/KgToPoundsEquivalenceTest.m
+++ tests/KgToPoundsEquivalenceTest.m
@@ -11,7 +11,7 @@ classdef KgToPoundsEquivalenceTest < matlabtest.compiler.TestCase
     
     methods(Test, TestTags = {'EquivalenceTest'})
         function pythonEquivalenceTest(testCase)
-            loadedData = load("pythonBuild.mat");
+            loadedData = load("../pythonBuild.mat");
             buildResults = loadedData.buildResults;
             executionResults = execute(testCase,buildResults,{[1,2,3,4,5]});
             verifyExecutionMatchesMATLAB(testCase,executionResults);
