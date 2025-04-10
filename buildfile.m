dc057df55c4a3f849da8df426f61d31054a64df1
tsharmaMW <tsharma@mathworks.com>
2025-04-10 12:44:33 +0530

    update buildfile to have dependencies

--- buildfile.m
+++ buildfile.m
@@ -1,22 +1,32 @@
 function plan = buildfile
-import matlab.buildtool.tasks.TestTask;
-import matlab.buildtool.tasks.MexTask;
+import matlab.buildtool.tasks.*;
 
 % Create a plan from task functions
 plan = buildplan(localfunctions);
 
+% Add a task to delete task outputs and traces
+plan("clean") = CleanTask;
+
 % Add a task to build a MEX file
 plan("mex") = MexTask("src/arrayProduct.c","toolbox");
+plan("mex").Outputs = "toolbox/arrayProduct*";
 
 % Add a task to run tests and generate test and coverage results
 plan("test") = TestTask("tests/arrayProductTest.m", TestResults="test-results/results.xml");
-plan("test").Dependencies = ["mex"];
+plan("test").Dependencies = "mex";
+
+% Add a task to package the toolbox   
+plan("packageToolbox").Dependencies = "test";
+plan("packageToolbox").Inputs = plan("mex").Outputs;
+plan("packageToolbox").Outputs = "toolbox.mltbx";
+plan("packageToolbox").Description = "Package toolbox";
+
+plan.DefaultTasks = "packageToolbox";
 
 end
 
 function packageToolboxTask(~)
     % Create an mltbx toolbox package
-    dir toolbox/*.mex*;
     identifier = "arrayProduct";
     toolboxFolder = "toolbox";
     opts = matlab.addons.toolbox.ToolboxOptions(toolboxFolder,identifier);
